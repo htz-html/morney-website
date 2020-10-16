e393ecb3a2fbc60ab47686f108af654edd6ad209
htz-html <1006676722@qq.com>
2020-10-16 16:51:16 +0800

    deploy

---
+++ precache-manifest.941626a866eee027aa3c9e1719531dc0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "669cdf48d8ab67d51903",
+    "url": "/morney-website/css/app.4d5aabc8.css"
+  },
+  {
+    "revision": "98d3217e422cb9cfc6db9a10b65baa6b",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "669cdf48d8ab67d51903",
+    "url": "/morney-website/js/app.a4bf8de8.js"
+  },
+  {
+    "revision": "5a0b524c88f6e687d215",
+    "url": "/morney-website/js/chunk-vendors.eb79fbb6.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "c24e00a7ef3be1749eb0cd8f8aba94e3",
+    "url": "/morney-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
